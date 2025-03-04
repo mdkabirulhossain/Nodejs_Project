1e0316f74e98330623a894bc565fe950e89b4209
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-03-04 20:25:02 +0600

    Node js part 2

---
+++ Node.js
@@ -0,0 +1,19 @@
+//The founder of Node.js is Ryan Dah
+
+//#1
+
+// JavaScript Runtime
+
+// i. Design the Language
+
+// function myFunc(){ // We also write JS function multiple ways
+//     console.log("Hello world");
+// }
+
+
+// ii. Execute the Language
+
+// Create JavaScript engine for browser
+// Each browser has its own JavaScript engine, with Chrome using V8, Firefox using SpiderMonkey, and Safari using JavaScriptCore. 
+// Node js is not a programming Language
+// Node js is a C++ program. We are able to use Node js in any machine
