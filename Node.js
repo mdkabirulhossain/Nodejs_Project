9e6f78fe76123d60bb5866ff1efc221137ec7055
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-03-04 22:45:56 +0600

    Node js core concept Non-blocking I/O and Asyncronous

--- Node.js
+++ Node.js
@@ -18,3 +18,15 @@
 // Node js is not a programming Language
 // Node js is a C++ program. We are able to use Node js in any machine
 // So Node js is a javaScript runtime powerd by V8 engine and which is also run in  out side the browser that means web server
+//Extra knowledge for Java use Tomcat Server which are able to handle multiple request at a time
+
+//Non-blocking I/o
+// Non-blocking I/O means Node.js does not wait for one task to finish before moving to the next. It handles multiple tasks at the same time without stopping the program.
+
+// Example:
+
+// You order food at a restaurant. Instead of waiting for your food, you can talk to your friend.
+// When the food is ready, the waiter brings it to you.
+// Similarly, Node.js handles tasks in the background and continues working on other things.
+
+//Asyncronous (call back function)
