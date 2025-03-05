2e0bba7e9c9512438e4b0dd4e9f025df1d0d63a8
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-03-05 17:08:59 +0600

    loadash library

--- Node.js
+++ Node.js
@@ -32,3 +32,14 @@
 //Asyncronous (call back function) 
 
 // # part 3
+//Loadash 
+// What is Lodash?
+// Lodash is a JavaScript library that works on the top of underscore.js. It helps in working with arrays, strings, objects, numbers, etc. 
+// It provides us with various inbuilt functions and uses a functional programming approach which that coding in JavaScript easier to understand
+// because instead of writing repetitive functions, tasks can be accomplished with a single line of code. It also makes it easier to work with 
+// objects in JavaScript if they require a lot of manipulation to be done on them.
+
+// Why Lodash?
+// It provides various inbuilt functions for collections, arrays, manipulated objects, and other utility methods that we can use directly instead 
+// of writing them from scratch. It makes it easier to iterate over the arrays, strings as well as objects. Its modular methods make the creation 
+// of composite functions easier.
