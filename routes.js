a909603b75f0be4b00f41c6bed069ef0b6ff25c6
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-03-16 23:19:14 +0600

    Create new useHandler Route

--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@ const{sampleHandler} = require('./handlers/routeHandlers/sampleHandler')
 //routes
 const routes = {
     sample : sampleHandler,
+    user : userHandler,
 }
 
 module.exports = routes;
